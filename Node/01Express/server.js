9a7e66c29c411bf646c1dfe2e1cf2ec6cd038703
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-08-25 23:38:43 +0530

    express server
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

---
+++ Node/01Express/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const port = 3000 || process.env.PORT ;
+
+
+app.get('/',(request ,response)=>{
+    response.send('Hello World');
+})
+
+app.get('/about',(request, response)=>{
+    response.send("<h1>I am about Page</h1>")
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('You can reach me at <a href ="http://www.google.com" >birmaan.puneet19@gmail.com</a>');
+})
+
+app.get('/services',(req,res)=>{
+    res.send(' <h1>Services Offered </h1> <br> <ul><li><h2>Sarcasm</h2></li><li><h2>More Sarcasm</h2></li><li><h2> Even more Sarcasm</h2></li></ul>');
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running at port : ${port}`);
+})
